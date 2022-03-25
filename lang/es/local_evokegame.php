d0ab307f7ff41a0795d36042befdcfeb4f656649
John Moravec <51122406+educationfutures@users.noreply.github.com>
2022-03-25 09:19:57 -0500

    Create local_evokegame.php

---
+++ lang/es/local_evokegame.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Lang file.
+ *
+ * @package     local_evokegame
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @author      Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Evoke Game';
+
+$string['customfields'] = 'Evoke Game - Activity custom fields';
+
+$string['scoreboard'] = 'Marcador';
+$string['backtomission'] = 'Volver a las misiones';
+$string['totheleaderboard'] = 'Ir a la tabla de posiciones';
+$string['backtopersonalscore'] = 'Volver a puntaje personal';
+$string['collectedsuperpowers'] ='Superpoderes acumulados';
+$string['myteams'] = 'Mi equipo';
+$string['mysuperpowers'] = 'Mis superpoderes';
+$string['myskills'] = 'Mis habilidades';
+$string['points'] = 'Puntos';
+$string['viewscoreboard'] = 'Ver marcador';
+$string['chooseavatar'] = 'Elige tu avatar';
+$string['chooseavatar_success'] = 'Avatar actualizada con éxito.';
+$string['badgessettings'] = 'Evoke badges settings';
+$string['createbadge'] = 'Create badge';
+$string['name'] = 'Name';
+$string['actions'] = 'Actions';
+$string['relatedbadge'] = 'Related badge';
+$string['deleteitem_confirm_title'] = 'Are you sure?';
+$string['deleteitem_confirm_msg'] = 'Once deleted, this item cannot be recovered!';
+$string['deleteitem_confirm_yes'] = 'Yes, delete it!';
+$string['deleteitem_confirm_no'] = 'Cancel';
+$string['createbadge_success'] = 'Evoke badge successfully created';
+$string['editbadge_success'] = 'Evoke badge successfully updated';
+$string['deletebadge_success'] = 'Evoke badge successfully deleted';
+$string['editbadge'] = 'Edit badge';
+
+$string['subplugintype_evokebadgecriteria'] = 'Evoke badge criteria method';
+$string['subplugintype_evokebadgecriteria_plural'] = 'Evoke badge criterias methods';
+$string['createbadgecriteria'] = 'Create badge criteria';
+$string['criteriamethod'] = 'Criteria method';
+$string['criteriatarget'] = 'Criteria target';
+$string['criteriavalue'] = 'Criteria value';
+$string['value'] = 'Value';
+$string['chooseanoption'] = 'Choose an option';
+$string['subplugintype_evokebadgecriteria_skillpoints'] = 'Skill points';
+$string['subplugintype_evokebadgecriteria_courseaccess'] = 'Course access';
+$string['subplugintype_evokebadgecriteria_skillpointsaggregation'] = 'Skill points aggregation';
+$string['createbadgecriteria_success'] = 'Criteria successfully created';
+$string['deletebadgecriteria_success'] = 'Criteria successfully deleted';
+
+$string['eventpoints_added'] = 'Points added event';
+$string['congratulations'] = 'Congratulations!';
+$string['youveearnedabadge'] = 'You\'ve earned a badge!';
+$string['youveearnedanachievement'] = 'You\'ve earned an achievement!';
+$string['youjustearnedanewbadge'] = 'You just earned a new badge!';
+$string['youjustearnedanewachievement'] = 'You just earned a new achievement!';
+$string['youjustearnedanewbadge_desc'] = 'For outstanding work, you\'ve earned the <b><i>{$a}</i></b> badge';
+$string['youjustearnedanewachievement_desc'] = 'For outstanding work, you\'ve earned the <b><i>{$a}</i></b> achievement';
+$string['checkyourscoreboard'] = 'Check your score board';
+
+$string['badgetype'] = 'Badge type';
+$string['badgetype_badge'] = 'Insignia';
+$string['badgetype_achievement'] = 'Logro';
+$string['myachievements'] = 'Mis logros';
+$string['progressdidnotstarted'] = 'Aún no tienes progreso';
+$string['progressdidnotstarted_desc'] = 'Vuelve a la página de la misión, finaliza las actividades e interactúa para recibir monedas virtuales y conseguir habilidades e insignias.';
+
+$string['updateavatar'] = 'Update avatar';
+$string['editprofile'] = 'Edit profile';
+$string['aboutme'] = 'About me:';
+$string['fullname'] = 'Fullname:';
+$string['email'] = 'Email:';
+
+$string['coursesettings'] = 'Course settings';
+$string['coursemenuitems'] = 'Course menu items';
+$string['coursemenuitems_help'] = 'A custom menu may be configured here. Enter each menu item on a new line with format: menu text, a link URL, and a uniquekey(only letter). For example:
+<pre>
+Announcements|/course/|announcements
+Team chat|/mod/chat/view.php?id=2|teamchat
+Course forum|/mod/forum/view.php?id=3|courseforum
+</pre>';
+$string['coursemenuitems_success'] = 'Course settings successfully saved.';
+$string['isgameenabledincourse'] = 'Is game enabled in this course?';
+
+$string['toastr_skillpoints'] = 'Acabas de recibir puntos de habilidad';
+$string['toastr_evocoins'] = 'Acabas de recibir evocoins';
