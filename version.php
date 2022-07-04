97079f3547550e35ffea63cbf48c3ade05e7ca79
Willian Mano <willianmanoaraujo@gmail.com>
2022-07-04 15:12:16 -0300

    Adds requirement of moodle 4.0

--- version.php
+++ version.php
@@ -13,5 +13,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_evokegame';
 $plugin->release = '1.1.0';
 $plugin->version = 2022061500;
-$plugin->requires = 2021051700;
+$plugin->requires = 2022041200;
 $plugin->maturity = MATURITY_STABLE;
