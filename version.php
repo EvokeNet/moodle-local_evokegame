be56395ff2b357c23f83e6c8e5de72e126f93b09
Willian Mano <willianmanoaraujo@gmail.com>
2022-09-15 16:53:02 -0300

    Increase version num

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_evokegame';
-$plugin->release = '1.2.0';
-$plugin->version = 2022090100;
+$plugin->release = '1.2.1';
+$plugin->version = 2022091500;
 $plugin->requires = 2022041200;
 $plugin->maturity = MATURITY_STABLE;
