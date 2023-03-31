64e4210a02f37eac8d76ec8cec71649b5ae2f304
Willian Mano <willian.mano@jove.com>
2023-03-31 13:15:42 -0300

    Add fr language

---
+++ lang/fr/local_evokegame.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Lang file.
+ *
+ * @package     local_evokegame
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @author      Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Jeu Evoke';
+
+$string['customfields'] = 'Jeu Evoke - Champs personnalisés d\'activité';
+
+$string['scoreboard'] = 'Tableau de bord';
+$string['backtomission'] = 'Retour à la mission';
+$string['totheleaderboard'] = 'Vers le classement';
+$string['backtopersonalscore'] = 'Retour au score personnel';
+$string['collectedsuperpowers'] =' Superpouvoirs collectés';
+$string['myteams'] = 'Mes équipes';
+$string['mysuperpowers'] = 'Mes superpouvoirs';
+$string['myskills'] = 'Mes compétences';
+$string['points'] = 'Points';
+$string['viewscoreboard'] = 'Afficher le tableau des scores';
+$string['chooseavatar'] = 'Choisissez votre avatar';
+$string['chooseavatar_success'] = 'Avatar enregistré avec succès.';
+$string['badgessettings'] = 'Les paramètres des badges Evoke';
+$string['createbadge'] = 'Créer un badge';
+$string['name'] = 'Nom ';
+$string['actions'] = 'Actions ';
+$string['deleteitem_confirm_title'] = 'Êtes-vous sûr?';
+$string['deleteitem_confirm_msg'] = 'Une fois supprimé, cet élément ne peut pas être récupéré!';
+$string['deleteitem_confirm_yes'] = 'Oui, supprimez-le!';
+$string['deleteitem_confirm_no'] = 'Annuler';
+$string['createbadge_success'] = 'Le badge Evoke a été créé avec succès';
+$string['editbadge_success'] = 'Le badge Evoke a été mis à jour avec succès';
+$string['deletebadge_success'] = 'Le badge Evoke a été supprimé avec succès';
+$string['editbadge'] = 'Modifier le badge';
+$string['deletebadge'] = 'Supprimer le badge';
+
+$string['badgedeliver'] = 'Livrer les badges';
+$string['badgecriteria'] = 'Critères du badge';
+$string['badgecriterias'] = 'Critères pour obtenir ce badge :';
+$string['deliverbadge_confirm_title'] = 'Êtes-vous sûr?';
+$string['deliverbadge_confirm_msg'] = 'Vous êtes sur le point de délivrer ce badge à tous les utilisateurs ayant atteint tous ses critères . Une fois délivré, le badge est irrévocable!';
+$string['deliverbadge_confirm_yes'] = 'Oui, livrez-le!';
+$string['deliverbadge_confirm_no'] = 'Annuler';
+$string['deliverbadge_success'] = 'Le badge Evoke a été livré avec succès pour {$a} utilisateurs.';
+$string['deliverbadge_badgenocriterias'] = 'Il n\'est pas possible de livrer ce badge car il n \'a pas de critère de livraison';
+
+$string['createbadgecriteria'] = 'Créer des critères de badge';
+$string['critèresmethod'] = 'Méthode des critères';
+$string['criterestarget'] = 'Cible des critères';
+$string['critèrevalue'] = 'Valeur des critères';
+$string['value'] = 'Valeur';
+$string['chooseanoption'] = 'Choisissez une option';
+$string['createbadgecriteria_success'] = 'Critères créés avec succès';
+$string['deletebadgecriteria_success'] = 'Critères supprimés avec succès';
+
+$string['eventpoints_added'] = 'Points ajoutés événement';
+$string['eventevocoins_added'] = ' Evocoins ajouté événement';
+$string['congratulations'] = 'Félicitations!';
+$string['youveearnedabadge'] = 'Vous avez gagné un badge!';
+$string['youveearnedanachievement'] = 'Vous avez obtenu un succès!';
+$string['youjustearnedanewbadge'] = 'Vous venez de gagner un nouveau badge!';
+$string['youjustearnedanewachievement'] = 'Vous venez de remporter un nouveau succès!';
+$string['youjustearnedanewbadge_desc'] = 'Pour un travail exceptionnel, vous avez gagné le badge <b><i>{$a}</i ></b>';
+$string['youjustearnedanewachievement_desc'] = 'Pour un travail exceptionnel, vous avez mérité la récompense <b><i>{$a}</i></b>';
+$string['checkyourscoreboard'] = 'Vérifiez votre tableau de bord';
+
+$string['badgetype'] = 'Type de badge';
+$string['badgetype_badge'] = 'Badge';
+$string['badgetype_achievement'] = 'Réussite';
+$string['highlight'] = 'Sélectionner ce badge?';
+$string['myachievements'] = 'Mes réussite';
+$string['progressdidnotstarted'] = 'Votre progression n\'a pas encore commencé';
+$string['progressdidnotstarted_desc'] = 'Retournez à la page de la mission, terminez les activités et interagissez pour recevoir des evocoins et obtenir des compétences et des badges.';
+
+$string['updateavatar'] = 'Mettre à jour l\'avatar';
+$string['editprofile'] = 'Modifier le profil';
+$string['aboutme'] = 'À propos de moi: ';
+$string['fullname'] = 'Nom et Prénom: ';
+$string['email'] = 'Email: ';
+
+$string['coursesettings'] = 'Les paramètres de cours du jeu Evoke';
+$string['coursesettings_success'] = 'Paramètres du cours enregistrés avec succès.';
+$string['isgameenabledincourse'] = 'Le jeu est-il activé dans ce cours?';
+
+$string['toastr_skillpoints'] = 'Vous venez de recevoir des points de compétence';
+$string['toastr_evocoins'] = 'Vous venez de recevoir {$a} evocoins';
+
+$string['scoreboard_finishdate'] = 'Date de fin du tableau de bord';
+$string['scoreboard_finishdate_help'] = 'Après cette date, le tableau de bord sera fermé.';
+$string['scoreboard_image'] = 'Image du tableau de bord';
+$string['scoreboard_prize'] = 'Titre du prix du tableau des scores';
+$string['scoreboard_endsin'] = 'Se termine par: ';
+$string['scoreboard_agent'] = 'Agent';
+$string['scoreboard_evc'] = 'EVC';
+$string['scoreboard_powers'] = 'Pouvoirs';
+$string['scoreboard_badges'] = 'Badges';
